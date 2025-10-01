2a231503daf4bbf4fffabe1e724806ee9bc88959
kennzuki <kingsyken@gmail.com>
2025-10-01 15:58:11 +0300

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+import logo from '../logo.svg'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div className="text-center">
+     ken
+    </div>
+  )
+}
