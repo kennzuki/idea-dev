09e0c47d666403dc3810fa1fa661e004b23e7228
kennzuki <kingsyken@gmail.com>
2025-10-03 01:51:31 +0300

    idea card button boolean

--- src/component/IdeaCard.tsx
+++ src/component/IdeaCard.tsx
@@ -1,16 +1,34 @@
 import { Link } from '@tanstack/react-router';
 import type { Idea } from '@/types';
+import clsx from 'clsx';
 
-const IdeaCard = ({ idea }: { idea: Idea }) => {
-    return ( 
-        <>
-          <li key={idea.id} className='border text-gray-800 p-4 m-2 list-none'>
-            <h2 className='text-lg font-bold'>{idea.title}</h2>
-            <p className='mt-2'>{idea.summary}</p>
-            <Link to='/ideas/$ideaId' params={{ ideaId: idea.id.toString() }} className='text-blue-500'>View</Link>
-          </li>
-        </>
-     );
-}
- 
+const IdeaCard = ({
+  idea,
+  button = true,
+}: {
+  idea: Idea;
+  button?: boolean;
+}) => {
+  const linkClasses = clsx({
+    'text-blue-600 hover:underline mt-3': !button,
+    'text-center mt-4 inline-block bg-blue-600 text-white px-4 py-2 rounded hove:bg-blue-700 transition':
+      button,
+  });
+  return (
+    <>
+      <li key={idea.id} className='border text-gray-800 p-4 m-2 list-none'>
+        <h2 className='text-lg font-bold'>{idea.title}</h2>
+        <p className='mt-2'>{idea.summary}</p>
+        <Link
+          to='/ideas/$ideaId'
+          params={{ ideaId: idea.id.toString() }}
+          className={linkClasses}
+        >
+          {button ? 'View Idea' : 'Read More →'}
+        </Link>
+      </li>
+    </>
+  );
+};
+
 export default IdeaCard;
